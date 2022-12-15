2d4cd822d946f0d4ccaa1279bf49d6c1718db095
Bijaya Giri <jaya.jc999@gmail.com>
2022-12-15 13:15:24 +0545

    nother

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,17 @@
 <template>
   <link href="./dist/output.css" rel="stylesheet">
  <signIn/>
+ <secondSignIn/>
 </template>
 
 <script>
 import signIn from './components/signIn.vue'
+import secondSignIn from './components/secondSignIn.vue'
 export default {
   name: 'App',
   components: {
     signIn,
+    secondSignIn,
     
   }
 }
---
+++ src/components/secondSignIn.vue
@@ -0,0 +1,59 @@
+<template>
+  <head> </head>
+  <header>
+    <div class="bg-black h-screen w-full px-8 py-6 font-sans">
+      <div class="h-32"></div>
+      <h1 class="text-white font-bold text-4xl">Become a member </h1>
+    </div>
+    
+
+
+  <body>
+    <form>
+     <div class=" 
+              text-white
+              border-2
+              w-40
+              hover:border-blue-700
+              border-solid-blue
+              p-1
+              bg-[#141414]
+              shadow
+              appearance-none
+              my-2
+            ">
+      <input
+              class="bg-black w-20"
+              type="text"
+              placeholder="Username"
+              required
+            />
+
+
+     </div>
+
+
+
+    </form>
+    
+
+  </body>
+</header>
+</template>
+
+
+
+
+
+
+
+
+
+
+
+<script>
+export default {
+  name: "secondSignIn",
+};
+</script>
+
